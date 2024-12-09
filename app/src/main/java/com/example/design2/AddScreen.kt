f843f657224c5922be36ade95b28bd16529c282f
usman <usmanch2666@gmail.com>
2024-12-09 13:11:25 +0500

    design

--- app/src/main/java/com/example/design2/AddScreen.kt
+++ app/src/main/java/com/example/design2/AddScreen.kt
@@ -1,18 +1,90 @@
 package com.example.design2
 
+import android.app.DatePickerDialog
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.CalendarView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.design2.databinding.ActivityAddScreenBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class AddScreen : AppCompatActivity() {
-    lateinit var binding : ActivityAddScreenBinding
+class AddScreen : AppCompatActivity(), AdapterView.OnItemClickListener {
+    lateinit var binding: ActivityAddScreenBinding
+    private val calender = Calendar.getInstance()
+    private lateinit var sharedPreferences : SharedPreferences
+    var fruits = arrayOf(
+        "apple",
+        "banana",
+        "mango",
+        "pineapple",
+        "watermelon",
+        "strawberry",
+        "berry"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(binding.root)
         binding = ActivityAddScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+
+        binding.backbtn.setOnClickListener {
+            val intent = Intent(this@AddScreen,MainActivity::class.java)
+            startActivity(intent)
+        }
+        sharedPreferences = getSharedPreferences("AddScreenPrefs", Context.MODE_PRIVATE)
+
+        binding.calendarView.setOnClickListener {
+            showDatePicker()
+        }
+
+        val spin = binding.fruitSpinner
+        spin.onItemClickListener = this
+
+        val ad: ArrayAdapter<*> = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item, fruits
+        )
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spin.adapter = ad
+
+    }
+
+    private fun showDatePicker() {
+        val datePickerDialog = DatePickerDialog(
+            this, { DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
+                val selectedDate = Calendar.getInstance()
+                selectedDate.set(year, monthOfYear, dayOfMonth)
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                val formattedDate = dateFormat.format(selectedDate.time)
+                binding.tvDate.text = "Selected Date: $formattedDate"
+            },
+            calender.get(Calendar.YEAR),
+            calender.get(Calendar.MONTH),
+            calender.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
+    }
+
+
+    override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        Toast.makeText(
+            applicationContext, fruits[p2], Toast.LENGTH_LONG
+        ).show()
+
+        with(sharedPreferences.edit()) {
+            putString("selected_fruit", fruits.toString())
+            apply()
+        }
     }
 }
--- app/src/main/java/com/example/design2/MainActivity.kt
+++ app/src/main/java/com/example/design2/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.design2
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.Spinner
@@ -10,15 +13,43 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.design2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(binding.root)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableEdgeToEdge()
+
+        val sharedPreferences = getSharedPreferences("AddScreenPrefs", Context.MODE_PRIVATE)
+        val selectedFruits = sharedPreferences.getString("selectedFruit", "No SelectedFruits")
+        val selectedDate = sharedPreferences.getString("selected_date", "No date selected")
+
+        binding.icategory.text = "selected Fruit : $selectedFruits"
+        binding.idate.text = "selected Date : $selectedDate"
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.edit1.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddScreen::class.java)
+            startActivity(intent)
+        }
+
+        binding.edit2.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddScreen::class.java)
+            startActivity(intent)
+        }
+
+        binding.edit3.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddScreen::class.java)
+            startActivity(intent)
+        }
+        binding.edit4.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddScreen::class.java)
+            startActivity(intent)
+        }
+
+        binding.addbtn.setOnClickListener {
+            val intent = Intent(this@MainActivity,AddScreen::class.java)
+            startActivity(intent)
+        }
     }
-    val spinner: Spinner = findViewById(R.id.fruit_spinner)
-
-
-
 }
